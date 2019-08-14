d09795ce6680ceb58192c0b149e742cdfb8cf86f
zhiyan-liu <53815656+zhiyan-liu@users.noreply.github.com>
2019-08-14 19:53:59 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -12,26 +12,17 @@ from general_tools import *
 import python_speech_features as features
 	# https://github.com/jameslyons/python_speech_features
 
-##### 执行文件时目录下要有一个path_toke.txt，里面指定一个路径，其下建立一个train文件夹，放音频和音素文件 #####
 ##### 音频和音素文件用对齐程序输出的复制进来即可，不要改变文件名 #####
 ##### 可以处理多个文件，最后将所有结果输出到一个output.pkl文件 #####
 
 ##### THRESHOLD PARAMETER FOR VALID PHONEME JUDGEMENT #####
 ##### 为了数据更好，抛弃了太短的音素，下面的参数可以设置音素长度的下限值，单位为帧 #####
-frame_threshold = 10
+frame_threshold = 5
 ##### SCRIPT META VARIABLES #####
-VERBOSE = False
-DEBUG 	= False
-debug_size = 1
-	# Convert only a reduced dataset
-visualize = False
 
 data_type = 'float32'
 
-paths 				= path_reader('path_toke.txt')
-train_source_path	= os.path.join(paths[0], 'train')
-test_source_path	= os.path.join(paths[0], 'test')
-target_path			= os.path.join(paths[0], 'output')
+source_path	= 'test'
 
 phonemes = ['sil', 'dh', 'ae', 'r', 'sp', 'w', 'ah', 'z', 't', 'ay', 'm', 'hh', 'eh', 'n', 'l', 'ow', 'uw', 'g', 'd', 'p', 'ey', 's', 'k', 'ao', 'iy', 'f', 'ih', 'v', 'uh', 'er', 'aa', 'y', 'b', 'oy', 'zh', 'ng', 'aw', 'th']
 
@@ -47,11 +38,6 @@ def find_phoneme (phoneme_idx):
 	return -1
 
 def create_mfcc(method, filename):
-	"""Perform standard preprocessing, as described by Alex Graves (2012)
-	http://www.cs.toronto.edu/~graves/preprint.pdf
-	Output consists of 12 MFCC and 1 energy, as well as the first derivative of these.
-	[1 energy, 12 MFCC, 1 diff(energy), 12 diff(MFCC)
-	method is a dummy input!!"""
 	(rate,sample) = wav.read(filename)
 	mfcc = features.mfcc(sample, rate, winlen=0.023, winstep=0.01, numcep = 13, nfilt=26,	preemph=0.97, appendEnergy=True)
 	derivative = np.zeros(mfcc.shape)
@@ -60,25 +46,18 @@ def create_mfcc(method, filename):
 	out = np.concatenate((mfcc, derivative), axis=1)
 	return out, out.shape[0]
 
-def calc_norm_param(X, VERBOSE=False):
+def calc_norm_param(X):
 	total_len = 0
 	mean_val = np.zeros(X.shape[1])
 	std_val = np.zeros(X.shape[1])
-	for obs in X:
-		obs_len = obs.shape[0]
-		mean_val += np.mean(obs,axis=0)*obs_len
-		std_val += np.std(obs, axis=0)*obs_len
-		total_len += obs_len
+
+	X_len = X.shape[0]
+	mean_val += np.mean(X,axis=0)*X_len
+	std_val += np.std(X, axis=0)*X_len
+	total_len += X_len
 	
 	mean_val /= total_len
 	std_val /= total_len
-
-	if VERBOSE:
-		print(total_len)
-		print(mean_val.shape)
-		print('  {}'.format(mean_val))
-		print(std_val.shape)
-		print('  {}'.format(std_val))
 
 	return mean_val, std_val, total_len
 
@@ -93,8 +72,7 @@ def set_type(X, type):
 	return X
 
 
-def preprocess_dataset(phn_fname, wav_fname, VERBOSE=False, visualize=False):
-	"""Preprocess data, ignoring compressed files and files starting with 'SA'"""
+def preprocess_dataset(phn_fname, wav_fname):
 	i = 0
 	X = []
 	Y = []
@@ -123,7 +101,7 @@ def preprocess_dataset(phn_fname, wav_fname, VERBOSE=False, visualize=False):
 ##### PREPROCESSING #####
 X_output = np.empty(shape = [0,26])
 y_output = np.empty(shape = [0])
-for dirName, subdirList, fileList in os.walk(train_source_path):
+for dirName, subdirList, fileList in os.walk(source_path):
 	i = 0
 	for fname in fileList:
 		if not fname.endswith('.phn') or (fname.startswith("SA")): 
@@ -132,7 +110,7 @@ for dirName, subdirList, fileList in os.walk(train_source_path):
 		wav_fname = dirName + '/' + fname[0:-11] + '.wav'
 		i += 1
 		print('Preprocessing file' + str(i))
-		X, y, _ 	= preprocess_dataset(phn_fname, wav_fname , VERBOSE=False, visualize=False)									
+		X, y, _ =preprocess_dataset(phn_fname,wav_fname)									
 		print('File ' + str(i) + ' preprocessing complete')
 		print(X.shape)
 		mean_val, std_val, _ = calc_norm_param(X)
@@ -144,7 +122,7 @@ for dirName, subdirList, fileList in os.walk(train_source_path):
 print(X_output.shape)
 print(y_output.shape)
 
-with open(target_path + '/output.pkl', 'wb') as cPickle_file:
+with open('test.pkl', 'wb') as cPickle_file:
 	cPickle.dump(
 	[X_output, y_output], 
 	cPickle_file, 
