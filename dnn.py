21d4d21f0523e522c9acaf05e08ce4931732ca8d
zhiyan-liu <53815656+zhiyan-liu@users.noreply.github.com>
2019-08-16 13:07:20 +0800

    Update dnn.py

--- dnn.py
+++ dnn.py
@@ -1,33 +1,68 @@
 import numpy as np
-import keras
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers import Dense, Activation
-from keras.optimizers import RMSprop
+import tensorflow as tf
+from tensorflow import keras
+from six.moves import cPickle 
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 
-##### 先导入X_train与y_train #####
+train_data_path = 'train.pkl'
+validation_data_path = 'val.pkl'
+test_data_path = 'test.pkl'
 
-y_train = np_utils.to_categorical(y_train, num_classes=38)
-model = Sequential([
-	Dense(512, input_dim = 26),
-	Activation('sigmoid'),
-	Dense(425),
-	Activation('sigmoid'),
-	Dense(512),
-	Activation('sigmoid'),
-	Dense(38),
-	Activation('softmax'),
+##### Loading data #####
+with open(train_data_path, 'rb') as cPickle_file:
+	[X_train, y_train] = cPickle.load(cPickle_file)
+
+with open(validation_data_path, 'rb') as cPickle_file:
+	[X_val, y_val] = cPickle.load(cPickle_file)
+
+with open(test_data_path, 'rb') as cPickle_file:
+	[X_test, y_test] = cPickle.load(cPickle_file)
+
+print(X_train.shape)
+print(y_train.shape)
+print(X_val.shape)
+print(y_val.shape)
+print(X_test.shape)
+print(y_test.shape)
+
+y_train = keras.utils.to_categorical(y_train, num_classes=38)
+y_val = keras.utils.to_categorical(y_val, num_classes=38)
+y_test = keras.utils.to_categorical(y_test, num_classes=38)
+
+model = keras.Sequential([
+	keras.layers.Dense(1200, input_dim = 39),
+	keras.layers.Activation('relu'),
+	keras.layers.Dense(1200),
+	keras.layers.Activation('relu'),
+	keras.layers.Dense(1200),
+	keras.layers.Activation('relu'),
+	keras.layers.Dense(1200),
+	keras.layers.Activation('relu'),
+	keras.layers.Dense(1200),
+	keras.layers.Activation('relu'),
+	keras.layers.Dense(38),
+	keras.layers.Activation('softmax'),
 ])
 
-rmsprop = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+rmsprop = keras.optimizers.RMSprop(lr=0.00008, rho=0.9, epsilon=1e-08, decay=0.0)
 
 model.compile(optimizer=rmsprop,
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=2, batch_size=32)
+checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='auto', period=1)
+reducelr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1, mode='auto', cooldown=0, min_lr=0)
+callback_lists = [checkpoint, reducelr]
+history = model.fit(X_train, y_train, epochs=100, batch_size=64,verbose=1,validation_data=(X_val,y_val),callbacks=callback_lists)
 
 loss, accuracy = model.evaluate(X_test, y_test)
 
 print('test loss: ', loss)
 print('test accuracy: ', accuracy)
+
+import pickle
+file = open('./models/history.pkl', 'wb')
+pickle.dump(history.history, file)
+file.close()
